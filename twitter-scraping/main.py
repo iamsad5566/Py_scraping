afdeb83fddc68e387421e7759ff4d7736b9753e6
Yen kuang Chen <yenkuangchen@Yens-MacBook-Pro.local>
2022-09-24 12:36:00 +0800

    feat: first commit

---
+++ twitter-scraping/main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import os
+
+class TwitterPyScraping:
+    def __init__(self) -> None:
+        print("I am good")
+        
+        
+if __name__ == "__main__":
+    TwitterPyScraping()
