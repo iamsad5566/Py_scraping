3f1161ae41d88515f1f9732cf2c735d0001adca5
sam <sam-chen@eup.com.tw>
2022-09-26 17:20:41 +0800

    update: update main method

--- twitter-scraping/main.py
+++ twitter-scraping/main.py
@@ -1,9 +1,16 @@
+from processController import ProcessController
 
 class TwitterPyScraping:
     def __init__(self) -> None:
-        pass
-        
-        
+        self.process = ProcessController()
+    
+    def runSingleSearching(self):
+        exampleSearch = "Taiwan"
+        self.process.singleSearching(run=5, keyword=exampleSearch)
+    
+    def runMultipleSearching(self):
+        exampleSearchList = ["tw", "jp", "us", "uk"]
+        self.process.multipleSearching(run=5, keyword=exampleSearchList)
         
 if __name__ == "__main__":
     TwitterPyScraping()
