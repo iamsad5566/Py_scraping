f6f8a2aa1e4cb6c35423b5977a9ee7aa618639e3
Yen-Kuang Chen <nf8964p5566@gmail.com>
2022-10-09 11:21:16 +0800

    update: update 1009

--- component/sharedComponent.go
+++ component/sharedComponent.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 // horizontalSpace serving as the border to split two neighboured objects
@@ -13,11 +14,33 @@ var horizontalSpace string = "           " +
 	"                        "
 
 // title returns a canvasObject containing the label showing the current page's gist.
-func title(str string) fyne.CanvasObject {
+func tittle(str string) fyne.CanvasObject {
 	titleText := canvas.NewText(str, nil)
 	titleText.TextSize = 50
 	titleText.TextStyle.Bold = true
 	space := canvas.NewText(" ", color.White)
 	content := container.NewBorder(space, space, space, space, container.NewCenter(titleText))
+	return content
+}
+
+// tmp
+// TwitterLayout contains all the elements belonging to twitter related manipulations
+func TwitterLayout() fyne.CanvasObject {
+	title := tittle("Twitter scraper")
+
+	// Insert a selection
+	selectContent := widget.NewSelect(selects, func(s string) {
+		selection = &s
+	})
+
+	selectContainer := container.NewBorder(canvas.NewText("", color.Opaque),
+		canvas.NewText("", color.Opaque), canvas.NewText(horizontalSpace, color.Opaque),
+		canvas.NewText(horizontalSpace, color.Opaque), selectContent)
+
+	options := twiOption()
+	button := twiButton()
+
+	content := container.NewVBox(title, selectContainer, options, button)
+
 	return content
 }
--- component/twigui/searchByID.go
+++ component/twigui/searchByID.go
@@ -2,6 +2,9 @@ package twigui
 
 import (
 	"image/color"
+	"log"
+	"os/exec"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -10,13 +13,14 @@ import (
 )
 
 type SearchByID struct {
-	Choice     string
-	LabelText  string
-	EntryInput string
-	RunTime    string
+	Choice          string
+	LabelText       string
+	EntryInput      string
+	RunTime         string
+	HorizontalSpace string
 }
 
-var _ TwiGUI = (*SearchByID)(nil)
+var _ TwiPage = (*SearchByID)(nil)
 var option = []string{"Single searching", "Multiple searching"}
 
 func (s *SearchByID) SetChoice(str string) {
@@ -39,7 +43,7 @@ func (s *SearchByID) SetLabel(str string) {
 	s.LabelText = label
 }
 
-func (s *SearchByID) SetOption() fyne.CanvasObject {
+func (s *SearchByID) GetOptionContainer() fyne.CanvasObject {
 	label := canvas.NewText("Select a mode to execute:", color.NRGBA{60, 80, 255, 255})
 	label.TextSize = 20
 	space := canvas.NewText(" ", color.Opaque)
@@ -51,7 +55,7 @@ func (s *SearchByID) SetOption() fyne.CanvasObject {
 		s.SetChoice(str)
 
 		if len(str) > 0 {
-			entryBox.Add(s.SetEntries(str))
+			entryBox.Add(s.getEntriesContainer(str))
 		}
 
 		entryBox.Refresh()
@@ -63,7 +67,7 @@ func (s *SearchByID) SetOption() fyne.CanvasObject {
 	return content
 }
 
-func (s *SearchByID) SetEntries(str string) fyne.CanvasObject {
+func (s *SearchByID) getEntriesContainer(str string) fyne.CanvasObject {
 	s.SetLabel(str)
 	label := canvas.NewText(s.LabelText, nil)
 	label.TextSize = 15
@@ -82,7 +86,54 @@ func (s *SearchByID) SetEntries(str string) fyne.CanvasObject {
 	return content
 }
 
-func (s *SearchByID) Layout() fyne.CanvasObject {
-	tmp := container.NewVBox(canvas.NewText("", color.Opaque))
-	return tmp
+func (s *SearchByID) GetButton() fyne.CanvasObject {
+	notification := canvas.NewText("", color.Opaque)
+	notification.TextSize = 30
+	notification.TextStyle.Bold = true
+
+	button := widget.NewButton("              Go!             ", func() {
+		if s.Choice == "" {
+			notification.Text = "Please select a mode!"
+		} else if s.EntryInput == "" {
+			notification.Text = "Please fill in the ID!"
+		} else {
+			notification.Text = ""
+			notification.Color = color.Opaque
+			notification.Refresh()
+			s.twiExec()
+			return
+		}
+		notification.Color = color.RGBA{255, 0, 0, 255}
+		notification.Refresh()
+
+	})
+
+	space := canvas.NewText(" ", color.Opaque)
+	horizontal := canvas.NewText(s.HorizontalSpace, color.Opaque)
+	content := container.NewVBox(container.NewBorder(space, space, horizontal, horizontal, button), container.NewCenter(notification))
+	return content
+
+}
+
+func (s *SearchByID) twiExec() {
+	if s.EntryInput == "" || s.Choice == "" {
+		return
+	}
+
+	s.EntryInput = strings.ReplaceAll(s.EntryInput, " ,", ",")
+	s.EntryInput = strings.ReplaceAll(s.EntryInput, ", ", ",")
+
+	var args = []string{"twitter-scraping/main.py", s.Choice}
+	if s.RunTime != "" && s.RunTime != "0" {
+		args = append(args, s.RunTime)
+	}
+
+	IDs := strings.Split(s.EntryInput, ",")
+	args = append(args, IDs...)
+
+	cmd := exec.Command("venv/bin/python3", args...)
+	err := cmd.Run()
+	if err != nil {
+		log.Println(err)
+	}
 }
